2b6185c7d9abe94404d531ce3744416c0a58ed86
GlacierEQ <casey@HiClassHomeServices.onmicrosoft.com>
2026-05-16 23:31:32 -1000

    APEX: Production System Cleanup & Production Readiness

--- test-maximize.js
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-import { remoteExecutor } from './dist/lib/remoteExecutor.js';
-
-remoteExecutor.execute('kilo.maximize', {}).then(r => console.log(JSON.stringify(r, null, 2)));
